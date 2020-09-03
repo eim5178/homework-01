97f04e2d01aae18704e22aebf9c41d3afa47ad63
eim5178 <eim5178@psu.edu>
2020-09-03 14:54:44 +0000

    outputs

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ for i in range (3):
   elif course_grade == 'B-' :
     gp_course = float(2.67)
     print (f"Grade point for course {i+1} is: {gp_course}")
-  elif course_grade == 'C+' :
+  elif course_grade == 'C+':
     gp_course = float(2.33)
     print (f"Grade point for course {i+1} is: {gp_course}")
   elif course_grade == 'C' :
