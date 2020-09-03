5aa3188224202dd1a56adfab4f508eb54527cd5b
eim5178 <eim5178@psu.edu>
2020-09-03 14:44:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+calc_num = 0
+total_course_credit = 0 
+for i in range (3):
+  course_grade = input(f"Enter your course {i+1} grade: ")
+  course_credit = input(f"Enter your course {i+1} credit: ")
+  course_credit = float(course_credit)
+  if course_grade == 'A' :
+    gp_course = float(4.0)
+    print (f"Grade point for course {i} is: {gp_course}")
+  elif course_grade == 'A-' :
+    gp_course = float(3.67)
+    print (f"Grade point for course {i} is: {gp_course}")
+  elif course_grade == 'B+' :
+    gp_course = float(3.33)
+    print (f"Grade point for course {i} is: {gp_course}")
+  elif course_grade == 'B' :
+    gp_course = float(3.0)
+    print (f"Grade point for course {i} is: {gp_course}")
+  elif course_grade == 'B-' :
+    gp_course = float(2.67)
+    print (f"Grade point for course {i} is: {gp_course}")
+  elif course_grade == 'C+' :
+    gp_course = float(2.33)
+    print (f"Grade point for course {i} is: {gp_course}")
+  elif course_grade == 'C' :
+    gp_course = float(2.0)
+    print (f"Grade point for course {i} is: {gp_course}")
+  elif course_grade == 'C-' :
+    gp_course = float(1.67)
+    print (f"Grade point for course {i} is: {gp_course}")
+  elif course_grade == 'D' :
+    gp_course = float(1.0)
+    print (f"Grade point for course {i} is: {gp_course}")
+  elif course_grade == 'F' :
+    gp_course = float(0.0)
+    print (f"Grade point for course {i} is: {gp_course}")
+  else:
+    gp_course = float(0.0)
+    print (f"Grade point for course {i} is: {gp_course}")
+  calc_num = course_credit * gp_course + calc_num
+  total_course_credit = total_course_credit + course_credit
+calc_total = calc_num/total_course_credit
+print(f"Your GPA is: {calc_total}")
